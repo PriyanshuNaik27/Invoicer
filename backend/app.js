57be989718127b3fa4d02a3c8563f6b6f6dee15a
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-07-15 11:51:52 +0530

    connected mongo db

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+
+//routes import 
+// import userRouter from "./routes/userRouter.js"
+
+
+
+//routes declaration
+
+
+export default app;
